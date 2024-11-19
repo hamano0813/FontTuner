fa1e470d23a4a186b96c2b3b8ad300f05fbd8d36
Hamano0813 <hamano0813@gmail.com>
2024-11-19 22:24:05 +0800

    Refactor utils.py to improve Excel styling functions and add metadata saving capability

--- src/utils.py
+++ src/utils.py
@@ -1,41 +1,39 @@
 import pandas as pd
-from openpyxl.worksheet.worksheet import Worksheet
+from openpyxl.styles import Alignment, Border, Font, NamedStyle, Side
 from openpyxl.utils.cell import get_column_letter
-from openpyxl.styles import NamedStyle, Font, Alignment, Border, Side
+from openpyxl.worksheet.worksheet import Worksheet
+
+from weight import weight_df
 
 
-def format_sheet(writer, name: str, df: pd.DataFrame, header, table):
+def format_sheet(writer: pd.ExcelWriter, name: str, header: NamedStyle, table: NamedStyle):
     sheet: Worksheet = writer.sheets[name]
     for c_idx, row in enumerate(sheet.iter_rows()):
         for cell in row:
             cell.style = header if not c_idx else table
-    for c_idx, col in enumerate(df.columns):
-        column_width = max(8, df[col].astype(str).str.len().max(), df[col].name.__len__())
-        sheet.column_dimensions[get_column_letter(c_idx + 1)].width = column_width
 
 
-def generate_style():
+def init_style():
     side = Side(border_style="thin", color="000000")
     font = "Sarasa Gothic TC"
 
     header = NamedStyle("标题样式")
-    header.font = Font(name=font, bold=True, size=10)
+    header.font = Font(name=font, bold=True, size=9)
     header.alignment = Alignment(horizontal="center", vertical="center")
     header.border = Border(left=side, right=side, top=side, bottom=side)
     table = NamedStyle("数据样式")
-    table.font = Font(name=font, bold=False, size=10)
+    table.font = Font(name=font, bold=False, size=9)
     table.alignment = Alignment(vertical="center")
     table.border = Border(left=side, right=side, top=side, bottom=side)
 
     return header, table
 
 
-def save_temp_xlsx(data: dict[str, pd.DataFrame]):
-    writer = pd.ExcelWriter("temp.xlsx", engine="openpyxl")
-
-    header, table = generate_style()
-
-    for name, df in data.items():
-        df.to_excel(writer, sheet_name=name, index=False)
-        format_sheet(writer, name, df, header, table)
+def save_meta(metadata_df: pd.DataFrame):
+    writer = pd.ExcelWriter("metadata.xlsx", engine="openpyxl")
+    header, table = init_style()
+    metadata_df.to_excel(writer, sheet_name="metadata", index=False)
+    weight_df.to_excel(writer, sheet_name="weight", index=True)
+    format_sheet(writer, "metadata", header, table)
+    format_sheet(writer, "weight", header, table)
     writer.close()
