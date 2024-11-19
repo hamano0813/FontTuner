0fe95ab31a28b6c1c6f0c093a5e307f000eb15e0
Hamano0813 <hamano0813@gmail.com>
2024-11-19 22:24:18 +0800

    删除 ttf_metadata.py 文件并在 main.py 中重构以使用 metadata 模块处理字体文件和 Excel 文件

--- src/main.py
+++ src/main.py
@@ -1,30 +1,29 @@
 import os
 import sys
 
-import ttf_metadata
+import pandas as pd
+
+import metadata
 import utils
-
-
-def list_files(paths: list[str]):
-    paths_set = set()
-
-    for path in paths:
-        if os.path.isdir(path):
-            for root, _, files in os.walk(path):
-                for file in files:
-                    paths_set.add(os.path.join(root, file))
-        else:
-            paths_set.add(path)
-    return list(paths_set)
-
 
 if __name__ == "__main__":
     paths = sys.argv[1:]
     if not paths:
         print("please drag and drop font files or font directory to this script")
         sys.exit(1)
-    paths = list_files(paths)
 
-    ttf_name_df, ttf_os2_df = ttf_metadata.load_metadata(paths)
+    if paths[0].endswith(".xlsx"):
+        df = pd.read_excel(paths[0], engine="openpyxl", dtype=str, na_values=None, keep_default_na=False)
 
-    utils.save_temp_xlsx({"name": ttf_name_df, "os2": ttf_os2_df})
+        metadata.save(df)
+    else:
+        font_paths = []
+        for path in paths:
+            if os.path.isdir(path):
+                for root, _, files in os.walk(path):
+                    for file in files:
+                        font_paths.append(os.path.join(root, file))
+            else:
+                font_paths.append(path)
+        df = metadata.load([path for path in font_paths if path.lower().endswith((".ttf", ".otf"))])
+        utils.save_meta(df)
