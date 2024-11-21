8ad27be6b672b25632f9912506ac5408b0ed2ac0
Hamano0813 <hamano0813@gmail.com>
2024-11-21 21:52:39 +0800

    优化 load_metadata 和 save_metadata 函数，简化字体属性处理逻辑，增强对字体宽度和粗细的支持

--- src/metadata.py
+++ src/metadata.py
@@ -9,17 +9,14 @@ from constants import FONT_WEIGHT, FONT_WIDTH
 
 def load_metadata(font: TTFont):
     font_setting = {
+        "usWeightClass": font["OS/2"].usWeightClass,
         "fsSelection": f"{font["OS/2"].fsSelection:016b}",
         "usWidthClass": font["OS/2"].usWidthClass,
-        "usWeightClass": font["OS/2"].usWeightClass,
     }
 
     lang_set = set()
 
     for record in font["name"].names:
-        if record.nameID not in (1, 2, 3, 4, 5, 6, 7, 16, 17):
-            continue
-
         nameID = record.nameID
         platFormID = record.platformID
         platEncID = record.platEncID
@@ -64,22 +61,19 @@ def prepare_metadata(font: TTFont, font_setting: dict):
     for key in font_setting.keys():
         if key in ("fsSelection", "usWidthClass", "usWeightClass", "fontPath"):
             continue
-        nameID, platformID, platEncID, langID = key
-        if nameID == 16:
-            if not (p_family := font_setting.get((16, platformID, platEncID, langID), "")):
-                font["name"].removeNames(platformID=platformID, platEncID=platEncID, langID=langID)
-            else:
-                langIDs.add((platformID, platEncID, langID))
-                if not font_setting.get((17, platformID, platEncID, langID), ""):
-                    weight = font["OS/2"].usWeightClass
-                    width = font["OS/2"].usWidthClass
-                    if p_family.isascii():
-                        sub_str = f"{FONT_WIDTH.get(width, ("", ""))[0]} " if width != 5 else ""
-                        sub_str += FONT_WEIGHT.get(weight, ("", ""))[0]
-                    else:
-                        sub_str = f"{FONT_WIDTH.get(width, ("", ""))[1]} " if width != 5 else ""
-                        sub_str += FONT_WEIGHT.get(weight, ("", ""))[1]
-                    font_setting[(17, platformID, platEncID, langID)] = sub_str
+        _, platformID, platEncID, langID = key
+        if (platformID, platEncID, langID) in langIDs:
+            continue
+        width = font["OS/2"].usWidthClass
+        weight = font["OS/2"].usWeightClass
+        if not (p_family := font_setting.get((16, platformID, platEncID, langID))):
+            font["name"].removeNames(platformID=platformID, platEncID=platEncID, langID=langID)
+        else:
+            langIDs.add((platformID, platEncID, langID))
+            if not font_setting.get((17, platformID, platEncID, langID)):
+                width_str = FONT_WIDTH.get(width, ("", ""))[p_family.isascii() ^ 1]
+                weight_str = FONT_WEIGHT.get(weight, ("", ""))[p_family.isascii() ^ 1]
+                font_setting[(17, platformID, platEncID, langID)] = " ".join([width_str, weight_str]).strip()
 
     langIDs = list(langIDs)
     return langIDs
@@ -125,28 +119,58 @@ def fetch_metadata(font: TTFont, font_setting: dict, langIDs):
 
 def save_metadata(font_setting: dict):
     font = TTFont(font_setting["fontPath"])
-    for key, value in font_setting.items():
-        if key == "fsSelection":
-            font["head"].macStyle = (font["head"].macStyle & ~(1 << 1)) | ((value & (1 << 0)) << 1)
-            font["head"].macStyle = (font["head"].macStyle & ~(1 << 2)) | ((value & (1 << 1)) << 1)
-            font["head"].macStyle = (font["head"].macStyle & ~(1 << 3)) | (value & (1 << 3))
-            font["head"].macStyle = (font["head"].macStyle & ~(1 << 0)) | ((value & (1 << 5)) >> 5)
-            if value & (1 << 6):
-                value &= ~0b00111111
-                font["head"].macStyle &= ~0b00111111
-            font["OS/2"].fsSelection = value
-        elif key == "usWeightClass":
-            font["OS/2"].usWeightClass = value
-        elif key == "usWidthClass":
-            font["OS/2"].usWidthClass = value
-            if value < 5:
-                font["head"].macStyle |= (1 << 4)
-                font["head"].macStyle &= ~(1 << 5)
-            elif value > 5:
-                font["head"].macStyle |= (1 << 5)
-                font["head"].macStyle &= ~(1 << 4)
-            else:
-                font["head"].macStyle &= ~((1 << 4) | (1 << 5))
+
+    weight = font_setting.get("usWeightClass", 400)
+    width = font_setting.get("usWidthClass", 5)
+    fs = font_setting.get("fsSelection", 64)
+    mac = font["head"].macStyle
+
+    if weight == 700:  # Bold
+        fs |= 1 << 5  # Set Bold
+        fs &= ~(1 << 6)  # Clear Regular
+        mac |= 1 << 0  # Set Bold
+    else:
+        fs &= ~(1 << 5)  # Clear Bold
+        mac &= ~(1 << 0)  # Clear Bold
+    mac = (mac & ~(1 << 1)) | ((fs & (1 << 0)) << 1)  # Set Italic
+    if width < 5:
+        mac |= 1 << 5  # Set Condensed
+        mac &= ~(1 << 6)  # Clear Extended
+    elif width > 5:
+        mac |= 1 << 6  # Set Extended
+        mac &= ~(1 << 5)  # Clear Condensed
+    else:
+        mac &= ~((1 << 5) | (1 << 6))  # Clear Condensed and Extended
+
+    font["OS/2"].usWeightClass = weight
+    font["OS/2"].fsSelection = fs
+    font["OS/2"].usWidthClass = width
+    font["head"].macStyle = mac
+
+    # for key, value in font_setting.items():
+    #     if key == "usWeightClass":
+    #         font["OS/2"].usWeightClass = value
+    #         if value == 700:
+    #             font["head"].macStyle |= 1 << 0
+    #     elif key == "fsSelection":
+    #         if value & (1 << 6):
+    #             value &= ~0b00111111
+    #             font["head"].macStyle &= ~0b00111111
+    #         font["OS/2"].fsSelection = value
+    #         font["head"].macStyle = (font["head"].macStyle & ~(1 << 1)) | ((value & (1 << 0)) << 1)
+    #         font["head"].macStyle = (font["head"].macStyle & ~(1 << 2)) | ((value & (1 << 1)) << 1)
+    #         font["head"].macStyle = (font["head"].macStyle & ~(1 << 3)) | (value & (1 << 3))
+    #         font["head"].macStyle = (font["head"].macStyle & ~(1 << 0)) | ((value & (1 << 5)) >> 5)
+    #     elif key == "usWidthClass":
+    #         font["OS/2"].usWidthClass = value
+    #         if value < 5:
+    #             font["head"].macStyle |= 1 << 4
+    #             font["head"].macStyle &= ~(1 << 5)
+    #         elif value > 5:
+    #             font["head"].macStyle |= 1 << 5
+    #             font["head"].macStyle &= ~(1 << 4)
+    #         else:
+    #             font["head"].macStyle &= ~((1 << 4) | (1 << 5))
 
     langIDs = prepare_metadata(font, font_setting)
     fetch_metadata(font, font_setting, langIDs)
