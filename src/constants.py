90252f70e9a0ff6a08ce10845f7f9e98c1630e3f
HamanoTaiki <hamano0813@gmail.com>
2024-11-27 15:33:31 +0800

    更新字体常量，修正汉字描述中的繁简体差异，优化字体宽度和样式的中文翻译

--- src/constants.py
+++ src/constants.py
@@ -5,7 +5,7 @@ FONT_WEIGHT = {
     300: ("Light", "细体", "細體"),
     325: ("SemiLight", "准细", "準細"),
     350: ("DemiLight", "半细", "半細"),
-    375: ("Book", "标准", "标准"),
+    375: ("Book", "标准", "標準"),
     400: ("Regular", "常规", "常規"),
     500: ("Medium", "中等", "中等"),
     600: ("DemiBold", "半粗", "半粗"),
@@ -18,11 +18,11 @@ FONT_WEIGHT = {
 }
 
 FONT_WIDTH = {
-    1: ("UltraCondensed", "极窄"，"極窄"),
+    1: ("UltraCondensed", "极窄", "極窄"),
     2: ("ExtraCondensed", "特窄", "特窄"),
     3: ("Condensed", "窄", "窄"),
     4: ("SemiCondensed", "半窄", "半窄"),
-    5: ("", ""),
+    5: ("Normal", "正常", "正常"),
     6: ("SemiExtended", "半宽", "半宽"),
     7: ("Extended", "宽", "宽"),
     8: ("ExtraExtended", "特宽", "特宽"),
@@ -30,21 +30,22 @@ FONT_WIDTH = {
 }
 
 FONT_STYLE = {
-    0b1 << 0: ("Italic", "斜体"),
-    0b1 << 1: ("Underline", "下划线"),
-    0b1 << 2: ("Negative", "反色"),
-    0b1 << 3: ("Outlined", "轮廓"),
-    0b1 << 4: ("Strikeout", "删除线"),
-    0b1 << 5: ("Bold", "粗体"),
-    0b1 << 6: ("Regular", "正常"),
+    0b1 << 0: ("Italic", "斜体", "斜體"),
+    0b1 << 1: ("Underline", "下划线", "下劃線"),
+    0b1 << 2: ("Negative", "反色", "反色"),
+    0b1 << 3: ("Outline", "轮廓", "輪廓"),
+    0b1 << 4: ("Strikeout", "删除线", "刪除線"),
+    0b1 << 4: ("Shadow", "阴影", "陰影"),
+    0b1 << 5: ("Bold", "粗体", "粗體"),
+    0b1 << 6: ("Regular", "正常", "正常"),
 }
 
 MAC_STYLE = {
-    0b1 << 0: ("Bold", "粗体"),
-    0b1 << 1: ("Italic", "斜体"),
-    0b1 << 2: ("Underline", "下划线"),
-    0b1 << 3: ("Outline", "轮廓"),
-    0b1 << 4: ("Shadow", "阴影"),
-    0b1 << 5: ("Condensed", "窄体"),
-    0b1 << 6: ("Extended", "宽体"),
+    0b1 << 0: ("Bold", "粗体", "粗體"),
+    0b1 << 1: ("Italic", "斜体", "斜體"),
+    0b1 << 2: ("Underline", "下划线", "下劃線"),
+    0b1 << 3: ("Outline", "轮廓", "輪廓"),
+    0b1 << 4: ("Shadow", "阴影", "陰影"),
+    0b1 << 5: ("Condensed", "窄体", "窄體"),
+    0b1 << 6: ("Extended", "宽体", "寬體"),
 }
